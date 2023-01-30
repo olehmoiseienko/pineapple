36ddd7253a39276f259df618d2be8bfe8f380339
omoiseie <omoiseie@cisco.com>
2023-01-30 11:05:25 +0100

    add mock data

---
+++ src/mocks/predefinedNodes.ts
@@ -0,0 +1,8 @@
+export const predefinedNodes = [
+  { module: "cs-power", type: "selectorNode", label: "selectorNode" },
+  { module: "cs-power", type: "customNode", label: "customNode" },
+  { module: "cs-power", type: "dc", label: "dc" },
+  { module: "cs-trigger", type: "output", label: "output" },
+  { module: "cs-trigger", type: "input", label: "input" },
+  { module: "cs-trigger", type: "default", label: "default" },
+];
