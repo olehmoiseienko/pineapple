5665585f9846059050e2c228cae2ba8898de9ed1
omoiseie <omoiseie@cisco.com>
2023-02-02 00:49:10 +0100

    fix Workspace layout

--- src/pipeline/styled/StyledWorkspace.tsx
+++ src/pipeline/styled/StyledWorkspace.tsx
@@ -4,7 +4,7 @@ export const StyledWorkspace = styled.div`
   flex-direction: row;
   display: flex;
   flex-grow: 1;
-  height: calc(100% - 50px);
+  height: 100%;
 `;
 
 export const StyledWorkspaceWrapper = styled.div`
