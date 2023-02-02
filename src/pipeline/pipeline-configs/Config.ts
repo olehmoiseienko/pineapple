1171a607894b71345ab9e971d6db740f9d92fea2
omoiseie <omoiseie@cisco.com>
2023-02-02 12:37:46 +0100

    add TODO

--- src/pipeline/pipeline-configs/Config.ts
+++ src/pipeline/pipeline-configs/Config.ts
@@ -1,6 +1,8 @@
 import staticConfigs, { registeredAllowedConfigsNames } from "./staticConfigs";
 
 class Config {
+
+  // TODO: Need to think about how improve providing custom config based on the Default config.
   getStaticConfig(configName: registeredAllowedConfigsNames) {
     return staticConfigs[configName];
   }
